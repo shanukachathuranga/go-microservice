b9f76c774ce59d1488220b414ff9e45576475ab4
shanuka chathuranga <shanukachathuranga21@gmail.com>
2025-07-29 18:02:41 +0530

    gitignore change

--- graphql/query_resolver.go
+++ graphql/query_resolver.go
@@ -6,10 +6,10 @@ type queryResolver struct {
 	server *Server
 }
 
-// func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string) ([]*Account, error) {
+func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string) ([]*Account, error) {
 
-// }
+}
 
-// func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput, query *string, id *string) ([]*Product, error) {
+func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput, query *string, id *string) ([]*Product, error) {
 
-// }
+}
