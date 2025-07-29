5d3f6a3cee37cadc915f81584e073171a6f418ef
shanuka chathuranga <shanukachathuranga21@gmail.com>
2025-07-29 18:05:15 +0530

    Revert "gitignore change"
    This reverts commit b9f76c774ce59d1488220b414ff9e45576475ab4.
    
    git revert

--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,19 +1,17 @@
 package main
 
-import "context"
-
 type mutationResolver struct {
 	server *Server
 }
 
-func (r *mutationResolver) createAccount(ctx context.context, in AccountInput) (*Account, error) {
+// func (r *mutationResolver) createAccount(ctx context.context, in AccountInput) (*Account, error) {
 
-}
+// }
 
-func (r *mutationResolver) createProduct(ctx context.context, in ProductInput) (*Product, error) {
+// func (r *mutationResolver) createProduct(ctx context.context, in ProductInput) (*Product, error) {
 
-}
+// }
 
-func (r *mutationResolver) createOrder(ctx context.context, in OrderInput) (*Order, error) {
+// func (r *mutationResolver) createOrder(ctx context.context, in OrderInput) (*Order, error) {
 
-}
+// }
